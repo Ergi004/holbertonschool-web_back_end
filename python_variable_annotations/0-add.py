0f1310561887d3178bffffe05149d9c348ad6946
Ergi Lama <ergilana98@gmail.com>
2024-01-05 16:39:19 +0100

    Fload a + b

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Simple module for strongly typed python
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    function that returns float a + b
+    """
+
+    return a + b
