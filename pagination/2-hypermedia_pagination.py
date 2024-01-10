cf1645b9a7986cf961b3f8a845dc8efbba7d1e70
Ergi Lama <ergilana98@gmail.com>
2024-01-10 14:18:30 +0100

    hypermedia

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -48,6 +48,22 @@ class Server:
         else:
             return []
 
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Return a dictionary with pagination information
+        """
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": page + 1 if page < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_logs.
+        }
+
 
 def index_range(page, page_size):
     """
