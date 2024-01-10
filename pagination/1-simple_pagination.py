8f4a9a4782ea1c9875e04586ff9a05be6b3b16e4
Ergi Lama <ergilana98@gmail.com>
2024-01-10 13:53:47 +0100

    simple Pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -33,12 +33,15 @@ class Server:
 
 
 def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-    assert isinstance(page, int) and page > 0, "page should be an integer, greater than 0"
-    assert isinstance(page_size, int) and page_size > 0, "page_size should be an integer, greater than 0"
-      
+    """
+    Get page method
+    """
+    assert isinstance(page, int) and page > 0, "page should be an integer greater than 0"
+    assert isinstance(page_size, int) and page_size > 0, "page_size should be an integer greater than 0"
+
     start_index, end_index = index_range(page, page_size)
-    return self.dataset()
-    [start_index:end_index] if start_index < len(self.dataset()) else []
+    return self.dataset()[start_index:end_index] if start_index < len(self.dataset()) else []
+
 
 
 def index_range(page, page_size):
