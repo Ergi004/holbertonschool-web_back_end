21a83188998d4f39e2319062f67fcb52c6301064
Ergi Lama <ergilana98@gmail.com>
2024-01-16 17:46:31 +0100

    Get full budget

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,4 +1,6 @@
+/*eslint-disable*/
 import getBudgetObject from './7-getBudgetObject.js';
+
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
