e06dd505143e19cc5649779debecfd6ac456752d
Ergi Lama <ergilana98@gmail.com>
2024-01-18 18:43:39 +0100

    Building class

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,18 +1,17 @@
 /*eslint-disable*/
 
 export default class Building {
-  constructor(sqft) {
-    if (this.evacuationWarningMessage === undefined) {
-      throw new TypeError('Class extending Building must override evacuationWarningMessage');
+    constructor(sqft) {
+        if (this._constructor === Building) {
+            throw new TypeError('Abstract class "Building" cannot be instantiated directly');
+        }
+        if (this._evacuationWarningMessage === undefined) {
+            throw new TypeError('Class extending Building must override evacuationWarningMessage');
+        }
+        this._sqft = sqft;
     }
-    this._sqft = sqft;
-  }
-    
+
     get sqft() {
-      return this._sqft;
+        return this._sqft;
     }
-    
-  evacuationWarningMessage() {
-    return 'Class extending Building must override evacuationWarningMessage';
-  }
 }
