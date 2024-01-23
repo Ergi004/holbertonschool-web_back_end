26fa28aa1b85106512e447b0519c3c3fda1928db
Ergi Lama <ergilana98@gmail.com>
2024-01-23 13:03:03 +0100

    First promise

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,6 +1,8 @@
 /*eslint-disable*/
-export default function getResponseFromAPI() {
-  return new Promise(() => {
-
+function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    if (promise) {
+      resolve("true");
+    }
   });
 }
