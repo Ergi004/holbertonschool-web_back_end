caa2373a6b0ae0604791c1d8721be0b53d98d5d3
Ergi Lama <ergilana98@gmail.com>
2024-01-25 16:58:11 +0100

    Upload photo

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,8 +1,7 @@
-/*eslint-disable*/
 export default function uploadPhoto(filename) {
   if (typeof fileName !== 'stirng') {
     return Promise.reject(
-        Error(`${filename} cannot be processed`)
+      Error(`${filename} cannot be processed`)
     );
   }
 }
