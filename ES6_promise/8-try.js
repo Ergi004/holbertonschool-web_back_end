24cf88b78e565cd934481ed763013503e14e3789
Ergi Lama <ergilana98@gmail.com>
2024-01-26 13:01:06 +0100

    Numeroator

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -2,6 +2,7 @@
 export default function divideFunction(numerator, denominator) {
   if (denominator === 0 && numerator / denominator) {
     throw new Error("cannot devide by 0");
+  } else {
+    return numerator / denominator;
   }
-  return numerator / denominator;
 }
