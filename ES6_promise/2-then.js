9119382a8ba0346c159c90acde37458e042a3e3a
Ergi Lama <ergilana98@gmail.com>
2024-01-26 11:15:47 +0100

    task 1 fix

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,11 +1,11 @@
 /*eslint-disable*/
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => {
+    .then((success) => {
         console.log('Got a response from the API');
         return {
             status: 200,
-            body: 'success'
+            body: success
         };
     })
     .catch(() => newError());
