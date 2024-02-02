3d82d2300394b69ffc43ef0e20a424ac18b6b124
Ergi Lama <ergilana98@gmail.com>
2024-02-02 10:38:32 +0100

    Clean set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,19 +1,21 @@
 /*eslint-disable*/
-export default function cleanSet(set, startString) {
-  if (startString === '') {
+function cleanSet(set, startString) {
+  if (typeof startString !== 'string' || startString === '') {
     return '';
   }
   
   let result = '';
   for (let item of set) {
     if (item.startsWith(startString)) {
-    if (result !== '') {
-          result += '-';
-    }
-    result += item.slice(startString.length);
+      if (result !== '') {
+        result += '-';
+      }
+      result += item.slice(startString.length);
     }
   }
   
   return result;
 }
+  
+export default cleanSet;
   
